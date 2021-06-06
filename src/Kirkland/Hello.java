936e72879d2a65d0427f8cafbfca7a22c0ee3079
Sean Kirkland <conconcraft@byui.edu>
2021-06-06 08:16:07 -0600

    Initial commit

---
+++ src/Kirkland/Hello.java
@@ -0,0 +1,13 @@
+package Kirkland;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Hello i = new Hello();
+        i.sayHello();
+    }
+
+    public void sayHello() {
+        System.out.println("Hello From Sean Kirkland!");
+    }
+}
